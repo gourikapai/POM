c93dce54b72a136c0754178c6646b884619d546d
gourikapai <gourikajun3@gmail.com>
2018-12-25 19:52:27 +0530

    Second commit

--- src/test/java/com/w2a/testcases/LoginTest.java
+++ src/test/java/com/w2a/testcases/LoginTest.java
@@ -16,6 +16,23 @@ import com.w2a.utilities.TestUtil;
 
 public class LoginTest extends BaseTest{
 	
+	/*How to commit and push code from eclipse to Git Hub
+	 * Precodition to be performed on git hub and on command prompt:
+	 * Git repository should be created in Git Hub 
+	 * go to project location C:\Users\vishw\Selenium\PageObjectModelFramework
+	 * right click->click on git bash here
+	 * type git init ,git status,git add <files>,git commit -m <message>,
+	 * git remote add origin github url ,
+	 * git push -u origin master etc commands on git command prompt
+	 * --------------------------------------------------
+	 * select project on eclipse ->right click on project->click on team->share project
+	 * then do some changed to your code 
+	 * then right click on project->click on team->click on add to index
+	 * then right click on project->click on team->click on commit->type commit message
+	 * click on commit and push
+	 * go to git hub repository and see your changes are getting reflected in git hub
+	 */
+	
 	@Test(dataProviderClass=TestUtil.class,dataProvider="dp")
 	public void loginTest(Hashtable<String,String> data)
 	{
