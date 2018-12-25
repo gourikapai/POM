92c711786864bb18093d3688e1c9e5587bfa9155
gourikapai <gourikajun3@gmail.com>
2018-12-25 19:59:11 +0530

    third commit

--- src/test/java/com/w2a/testcases/LoginTest.java
+++ src/test/java/com/w2a/testcases/LoginTest.java
@@ -16,21 +16,24 @@ import com.w2a.utilities.TestUtil;
 
 public class LoginTest extends BaseTest{
 	
-	/*How to commit and push code from eclipse to Git Hub
-	 * Precodition to be performed on git hub and on command prompt:
+	/*How to commit and push code from eclipse to Git Hub Repository
+	 * Precodition to be performed on git hub and on command prompt
 	 * Git repository should be created in Git Hub 
 	 * go to project location C:\Users\vishw\Selenium\PageObjectModelFramework
 	 * right click->click on git bash here
 	 * type git init ,git status,git add <files>,git commit -m <message>,
 	 * git remote add origin github url ,
 	 * git push -u origin master etc commands on git command prompt
+	 * And this is how the code will be pushed to git hub from command prompt
 	 * --------------------------------------------------
+	 * First precondition need be performed to get git related options under Team in eclipse
 	 * select project on eclipse ->right click on project->click on team->share project
-	 * then do some changed to your code 
+	 * then do some changes to your code 
 	 * then right click on project->click on team->click on add to index
 	 * then right click on project->click on team->click on commit->type commit message
 	 * click on commit and push
 	 * go to git hub repository and see your changes are getting reflected in git hub
+	 * And this is how the code changes will be committed and pushed to git hub from eclipse 
 	 */
 	
 	@Test(dataProviderClass=TestUtil.class,dataProvider="dp")
